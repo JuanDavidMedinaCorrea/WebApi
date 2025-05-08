3b24bda781c634d374c4fa7a28ec687c93069339
JuanDavidMedinaCorrea <juandavidmedina51@gmail.com>
2025-05-08 10:06:15 -0500

    parcial.4
    Agrege el modelo categorialog

---
+++ Models/CategoriaLog.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.Models
+{
+    public class CategoriaLog
+    {
+        public int Id { get; set; }
+        public int CategoriaIdEliminada { get; set; }
+        public DateTime FechaEliminacion { get; set; }
+        public string? Usuario { get; set; } 
+    }
+
+}
